b1a4c6e2025b53105cf5d3d36e61141e52881264
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2026-03-13 22:38:50 +0530

    Test Helper

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,14 @@
 ExUnit.start()
+
+Application.ensure_all_started(:bypass)
+
+Application.put_env(:aurinko, :client_id, "test_client_id")
+Application.put_env(:aurinko, :client_secret, "test_client_secret")
+Application.put_env(:aurinko, :timeout, 5_000)
+Application.put_env(:aurinko, :retry_attempts, 0)
+Application.put_env(:aurinko, :retry_delay, 10)
+Application.put_env(:aurinko, :cache_enabled, true)
+Application.put_env(:aurinko, :cache_ttl, 60_000)
+Application.put_env(:aurinko, :rate_limiter_enabled, false)
+Application.put_env(:aurinko, :circuit_breaker_enabled, false)
+Application.put_env(:aurinko, :attach_default_telemetry, false)
